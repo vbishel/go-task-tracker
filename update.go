9d0256311d6b191345498d26115b2c5596b67a5c
Victor Bishel <matodorrrrr@gmail.com>
2025-08-31 15:33:23 +0700

    Update task

--- update.go
+++ update.go
@@ -1,5 +1,42 @@
-package main;
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func Update() {
-	
+	if len(os.Args) < 4 {
+		log.Fatal("Expected task ID as first argument and task description as second argument")
+	}
+
+	taskID, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		log.Fatal("Incorret task ID")
+	}
+
+	tasks, file := GetTasksFromJSON()
+
+	defer func() {
+		err := file.Close()
+		if err != nil {
+			log.Fatal("Failed to close file")
+		}
+	}()
+
+	var newTasks Tasks = make(Tasks, 0, len(tasks))
+
+	for _, task := range tasks {
+		if task.ID == taskID {
+			task.Description = os.Args[3]
+		}
+
+		newTasks = append(newTasks, task)
+	}
+
+	newTasks.WriteToJSON(file)
+	fmt.Println("Task updated successfully")
 }
